1be0cbeb8d580f5b3c2cff491d24a0682d5a9dcc
olell <olel@olel.de>
2025-08-18 12:16:46 +0200

    🚀 made rendered frontend deployable

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from contextlib import asynccontextmanager
 import logging
-from fastapi import FastAPI
+from pathlib import Path
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from sqlmodel import Session
 
 from app.core.config import settings
@@ -45,3 +48,23 @@ if settings.all_cors_origins:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+build_path = Path("frontend/build")
+app.mount("/app", StaticFiles(directory=build_path), name="static")
+
+
+@app.middleware("http")
+async def fallback(request: Request, call_next):
+    if request.url.path.startswith("/app"):
+        relative_path = request.url.path.removeprefix("/app/").strip("/")
+        target_file = build_path / relative_path
+
+        if not target_file.exists() or target_file == build_path:
+            return FileResponse(build_path / "index.html")
+
+    return await call_next(request)
+
+
+@app.get("/")
+def index():
+    return RedirectResponse("/app/")
