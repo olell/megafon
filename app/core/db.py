323380b9d93c77390631e86ab904492b6a836b7d
olel <olel@olel.de>
2026-06-06 10:04:53 +0200

    🩹 use get_bind

--- app/core/db.py
+++ app/core/db.py
@@ -1,18 +1,18 @@
 from logging import getLogger
 from typing import Annotated
+
 from fastapi import Depends
-from sqlmodel import Session, create_engine, SQLModel
+from sqlmodel import Session, SQLModel, create_engine
 
 from app.core.config import settings
-
 
 logger = getLogger(__name__)
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 
 def init_db(session: Session) -> None:
-    SQLModel.metadata.create_all(engine)
-    logger.info(f"Created tables")
+    SQLModel.metadata.create_all(session.get_bind())
+    logger.info("Created tables")
 
 
 def drop_db(engine=engine) -> None:
