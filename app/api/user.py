053119812ad8e007bcecc2d9b4885df9c56e061b
olel <olel@olel.de>
2026-06-06 10:06:12 +0200

    🔒️ set token max age

--- app/api/user.py
+++ app/api/user.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from fastapi import APIRouter, Response
 import jwt
 from pydantic import BaseModel
@@ -19,11 +20,24 @@ class SessionInit(BaseModel):
 def init_session(*, session: SessionDep, data: SessionInit, response: Response) -> User:
     user = create_user(session, data.username)
 
-    token = jwt.encode(
-        {"sub": user.name, "uuid": str(user.id)}, settings.SECRET_KEY, algorithm="HS256"
+    max_age = settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
 
-    response.set_cookie(key="auth", value=token, expires=2147483647)
+    token = jwt.encode(
+        {"sub": user.name, "uuid": str(user.id), "exp": expire},
+        settings.SECRET_KEY,
+        algorithm="HS256",
+    )
+
+    response.set_cookie(
+        key="auth",
+        value=token,
+        max_age=max_age,
+        httponly=True,
+        samesite="lax",
+    )
     return user
 
 
