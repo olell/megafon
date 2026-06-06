197c1e93bddc0aab47332c070ecb0f375d46ccb4
olel <olel@olel.de>
2026-06-06 10:03:42 +0200

    🥅 narrowed error catch

--- app/api/deps.py
+++ app/api/deps.py
@@ -15,11 +15,12 @@ def get_current_user(
 ) -> User:
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        return get_user_by_id(session, payload.get("uuid"))
-    except Exception as e:
+    except jwt.PyJWTError:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated!"
         )
+
+    return get_user_by_id(session, payload.get("uuid"))
 
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
